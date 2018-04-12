5562fc9365abaff0ccd91bcbfadbb9db1f50cfe4
Yoonsik Park <park.yoonsik@icloud.com>
2018-04-12 02:00:01 -0400

    Fix formatting, compatibility with python3

--- pms3003/monitor.py
+++ pms3003/monitor.py
@@ -4,9 +4,9 @@ import g3
 air = g3.g3sensor()
 while True:
     try:
-        pmdata=air.read("/dev/ttyS1")
+        pmdata = air.read("/dev/ttyS1")
     except:
-        pmdata=[0,0,0,0,0,0]
-	continue
-    print pmdata
+        pmdata = [0,0,0,0,0,0]
+        continue
+    print(pmdata)
     time.sleep(1.0)
